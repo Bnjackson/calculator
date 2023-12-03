bca13236b07fa4b22bff8f3b30777d5107c4200f
Benjamin <bnjackson951@gmail.com>
2023-12-03 17:04:17 +0000

    Add structure to file, add global variables

---
+++ main.js
@@ -0,0 +1,32 @@
+const CLEAR_BTN = document.getElementById('clearBtn');
+const DELETE_BTN = document.getElementById('deleteBtn');
+const EQUALS_BTN = document.getElementById('equalsBtn');
+const SIGN_BTN = document.getElementById('signBtn');
+const OPERANDS = document.querySelectorAll('.operand-btn');
+const OPERATORS = document.querySelectorAll('.operators');
+
+let currentOperand = '0';
+let lastOperand = '';
+let currentOperator;
+
+function updateCurrentOperand() {
+
+}
+
+function updateLastOperand() {
+    
+}
+
+function updateCurrentOperator() {
+
+}
+
+function operate(operand1, operand2, operator) {
+
+}
+
+function clearCalculator() {
+
+}
+
+
